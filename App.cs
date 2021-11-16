cbc3377947d305c3cf39129e77e68a7507c3b270
Rely Kao <rely_kao@asiavista.com.tw>
2021-11-16 16:38:54 +0800

    Initial commit

---
+++ App.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Net5ConaoleApp.Services;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Net5ConaoleApp
+{
+    class App
+    {
+        readonly IConfiguration _config;
+        readonly CryptographyService _cypher;
+
+        public App(IConfiguration config, CryptographyService cypher)
+        {
+            _config = config;
+            _cypher = cypher;
+        }
+
+        /// <summary>
+        /// 取代原本 Program.Main() 函式的效用。
+        /// </summary>
+        public void Run(string[] args)
+        {
+            Dictionary<string, string> connList = new Dictionary<string, string>();
+            connList.Add("ConnDB", "我是連線字串1");
+            connList.Add("Conn2DB", "我是連線字串2");
+            connList.Add("Conn3DB", "我是連線字串3");
+            connList.Add("Conn4DB", "我是連線字串4");
+            connList.Add("Conn5DB", "我是連線字串1");
+            connList.Add("Conn6DB", "我是連線字串2");
+            connList.Add("Conn7DB", "我是連線字串3");
+            connList.Add("Conn8DB", "我是連線字串4");
+            connList.Add("Conn9B", "我是連線字串1");
+            connList.Add("ConnADB", "我是連線字串2");
+            connList.Add("ConnBDB", "我是連線字串3");
+            connList.Add("ConnCDB", "我是連線字串4");
+
+            string thumbprint = @"6CF5ACB9F5AF03741FF924A8542A0D108F453595";
+
+            var cryptoBlob = _cypher.EncryptDataOaepSha1(thumbprint, connList);
+            var dataBlob = _cypher.DecryptDataOaepSha1<Dictionary<string,string>>(thumbprint, cryptoBlob);
+
+            var signature = _cypher.SignData(thumbprint, connList);
+            bool isValid = _cypher.VerifyData(thumbprint, connList, signature);
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Services/CryptographyService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Net5ConaoleApp.Services
+{
+    class CryptographyService
+    {
+        readonly IConfiguration _config;
+        readonly ILogger<CryptographyService> _logger;
+
+        public CryptographyService(ILogger<CryptographyService> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public byte[] EncryptDataOaepSha256(X509Certificate2 cert, byte[] data)
+        {
+            using (RSA rsa = cert.GetRSAPublicKey())
+            {
+                return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
+
+        public byte[] DecryptDataOaepSha256(X509Certificate2 cert, byte[] data)
+        {
+            // GetRSAPrivateKey returns an object with an independent lifetime, so it should be
+            // handled via a using statement.
+            using (RSA rsa = cert.GetRSAPrivateKey())
+            {
+                return rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
+
+        /// <summary>
+        /// 用公鑰加密
+        /// </summary>
+        public byte[] EncryptDataOaepSha1(string thumbprint, object data)
+        {
+            var cert = FindCertInStore(StoreName.My, StoreLocation.CurrentUser, thumbprint);
+            if (cert == null) throw new ApplicationException("目標憑證不存在！");
+
+            var blob = JsonSerializer.SerializeToUtf8Bytes(data, data.GetType());
+            using (RSA rsa = cert.GetRSAPublicKey())
+            {
+                return rsa.Encrypt(blob, RSAEncryptionPadding.OaepSHA1);
+            }
+        }
+
+        /// <summary>
+        /// 用私鑰解密
+        /// </summary>
+        public T DecryptDataOaepSha1<T>(string thumbprint, byte[] cryptoBlob)
+        {
+            //## 私密金鑰只能放在【Cert:CurrentUser\My】位置。
+            var cert = FindCertInStore(StoreName.My, StoreLocation.CurrentUser, thumbprint);
+            if (cert == null) throw new ApplicationException("目標憑證不存在！");
+
+            using (RSA rsa = cert.GetRSAPrivateKey())
+            {
+                var dataBlob = rsa.Decrypt(cryptoBlob, RSAEncryptionPadding.OaepSHA1);
+                var data = JsonSerializer.Deserialize<T>(dataBlob);
+                return data;
+            }
+        }
+
+        public byte[] SignData(string thumbprint, object data)
+        {
+            var cert = FindCertInStore(StoreName.My, StoreLocation.CurrentUser, thumbprint);
+            if (cert == null) throw new ApplicationException("目標憑證不存在！");
+
+            var blob = JsonSerializer.SerializeToUtf8Bytes(data, data.GetType());
+            using (RSA rsa = cert.GetRSAPrivateKey())
+            {
+                return rsa.SignData(blob, HashAlgorithmName.SHA1, RSASignaturePadding.Pss);
+            }
+        }
+
+        public bool VerifyData(string thumbprint, object data, byte[] signature)
+        {
+            var cert = FindCertInStore(StoreName.My, StoreLocation.CurrentUser, thumbprint);
+            if (cert == null) throw new ApplicationException("目標憑證不存在！");
+
+            var blob = JsonSerializer.SerializeToUtf8Bytes(data, data.GetType());
+            using (RSA rsa = cert.GetRSAPrivateKey())
+            {
+                return rsa.VerifyData(blob, signature, HashAlgorithmName.SHA1, RSASignaturePadding.Pss);
+            }
+        }
+
+
+        static X509Certificate2 FindCertInStore(StoreName storeName, StoreLocation location, string thumbprint, bool validOnly = true)
+        {
+            using (X509Store store = new X509Store(storeName, location))
+            {
+                store.Open(OpenFlags.ReadOnly | OpenFlags.MaxAllowed | OpenFlags.IncludeArchived);
+                var result = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly);
+                if (result.Count > 0)
+                {
+                    return result[0];
+                };
+            }
+
+            //throw new ApplicationException($@"找不到目標憑證[Subject = {subject}]！");
+            return null;
+        }
+    }
+}
